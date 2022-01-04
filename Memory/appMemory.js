652824727a59efc93240a5713cdbbf3d68ce5704
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-01-04 16:36:34 +0100

    seconde carte condition(not fix)

--- Memory/appMemory.js
+++ Memory/appMemory.js
@@ -1,30 +1,31 @@
 let selectCarte = document.querySelectorAll('.double-face');
+let selectAllcarte = document.querySelectorAll('.carte');
 
-let carte_Attribut = document.querySelector('.double-face').getAttribute('data-attr');
 for (let cartes of selectCarte){
     cartes.addEventListener('click', tournerCarte);
     let carteRetourner = false;
     let carte_selected1;
     let carte_selected2;
+    let carteChoix = [];
+    let secondClick = false;
     function tournerCarte() {
-        if(this == carte_selected1){
-            this.classList.toggle('face');
-        }
         if(!carteRetourner){
+            console.log('carte 1');
+            this.classList.toggle('face');
             carteRetourner = true;
             carte_selected1 = this;
+            carteChoix.push(carte_selected1);
         }
-        carteRetourner = false;
-        carte_selected2 = this;
-        if(carte_selected1.getAttribute('data-attr') == carte_selected2.getAttribute('data-attr')){
-            carteRetourner =true;
-        }
-        else{
-            this.classList.toggle('arriere');
+        if(!secondClick){
+            console.log('secondclick');
+            carteRetourner = false;
+            carte_selected2 = this;
+            if(carte_selected2 !== carte_selected1){
+                this.classList.toggle('arriere');
+            }
         }
     }
 }
-
 //La carte se retourne au clic et dévoile au joueur le contenu de la carte. Celle-ci reste retournée.
 //Le choix de l’utilisateur est enregistré dans une variable.
 //Lorsque le joueur clic sur une seconde carte, celle-ci est comparée avec la précédente.
