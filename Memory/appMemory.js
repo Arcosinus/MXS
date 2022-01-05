f0892d683f051215c7b1fc91275cba0147100cd3
David <david.roquain@edu.esiee-it.fr>
2022-01-05 16:31:35 +0100

    Merge branch 'timer' into main + ajout score

--- Memory/appMemory.js
+++ Memory/appMemory.js
@@ -1,5 +1,37 @@
 const allCarte = document.querySelectorAll('.carte');
 const devoile = document.querySelectorAll('.double-face');
+const grille = document.querySelector('.grille');
+
+
+let divTimer = document.createElement("div");
+divTimer.className = "tmp";
+divTimer.style.cssText = 'text-align:center';
+divTimer.innerHTML ='<h1>0:00</h1>';
+document.body.insertBefore(divTimer, document.body.secondChild);
+
+const tmp = document.querySelector('h1');
+
+let min = 0o0;
+let seconds = 0o0;
+let splice = 0;
+let counter = setInterval(function(){
+    timer();
+},1000);
+function timer(){
+    seconds++;
+    if(seconds===59){
+        seconds = 0o0;
+        min = min + 1;
+    }
+    if(seconds===10){
+        splice = '';
+    }else{
+        if(seconds=== 0o0){
+            splice = '';
+        }
+    }
+    tmp.innerText = min + ':' + splice + seconds;
+}
 
 let carteTourner = false;
 let currCard;
@@ -37,7 +69,7 @@ function matchCarte() {
         carteBon++;
         console.log(carteBon);
         if (carteBon === 6) {
-            setTimeout( function(){ if (confirm('vous avez gagné !')){
+            setTimeout( function(){ if (confirm('vous avez gagné en ' + tmp.innerHTML + ' !')){
                 window.location.reload(false);
             } else {
             }}, 500)
