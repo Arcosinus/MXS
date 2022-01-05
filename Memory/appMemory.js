3de840d80d7b9f278b7d47f715a010269c8b8f2f
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-01-05 14:51:33 +0100

    Timer start auto (Not fix with card)

--- Memory/appMemory.js
+++ Memory/appMemory.js
@@ -1,5 +1,37 @@
 const allCarte = document.querySelectorAll('.carte');
 const devoile = document.querySelectorAll('.double-face');
+const grille = document.querySelector('.grille');
+
+
+let divTimer = document.createElement("div");
+divTimer.className = "tmp";
+divTimer.style.cssText = 'text-align:center';
+divTimer.innerHTML ='<h1>0:00</h1>';
+document.body.insertBefore(divTimer, document.body.secondChild);
+
+const tmp = document.querySelector('h1');
+
+let min = 0o0;
+let seconds = 0o0;
+let splice = 0;
+let counter = setInterval(function(){
+    timer();
+},1000);
+function timer(){
+    seconds++;
+    if(seconds===59){
+        seconds = 0o0;
+        min = min + 1;
+    }
+    if(seconds===10){
+        splice = '';
+    }else{
+        if(seconds=== 0o0){
+            splice = '';
+        }
+    }
+    tmp.innerText = min + ':' + splice + seconds;
+}
 
 let carteTourner = false;
 let currCard;
@@ -7,7 +39,6 @@ let prevCard;
 let carteChoix = [];
 
 devoile.forEach(card => card.addEventListener('click', flipCard));
-
 function flipCard() {
     this.classList.toggle('face');
     if (!carteTourner) {
@@ -35,27 +66,4 @@ function flipCard() {
             prevCard.classList.remove('face');
         }, 1000);
     }
-
-
 }
-
-
-
-
-
-
-/*if (!carteTourner) {
-       console.log('first')
-       carteTourner = true;
-       currCard = this;
-       carteChoix.push(currCard);
-       console.log('carte 1 ' + carteChoix);
-       return;
-   } else {
-   console.log('second')
-   carteTourner = false;
-   prevCard = this;
-   carteChoix.push(prevCard);
-   console.log('carte 2 ' + carteChoix);
-   return;
-   }*/
