0c8d4716fa424695151ea77aa533d14da8e59848
Lucas <tate.lucas@outlook.fr>
2022-01-05 15:15:16 +0100

    comparaison ok

--- Memory/appMemory.js
+++ Memory/appMemory.js
@@ -10,52 +10,35 @@ devoile.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard() {
     this.classList.toggle('face');
-    if (!carteTourner) {
         carteTourner = true;
         currCard = this;
-        carteChoix.push(this);
-        if (carteChoix.length === 2) {
-            setTimeout(() => {
-                if (carteChoix[0].innerHTML === carteChoix[1].innerHTML) {
-                    carteChoix[0].classList.add('match');
-                    carteChoix[1].classList.add('match');
-                    carteChoix = [];
-                } else {
-                    carteChoix[0].classList.remove('face');
-                    carteChoix[1].classList.remove('face');
-                    carteChoix = [];
-                }
-                carteTourner = false;
-            }, 1000);
-        }
-    } else {
-        prevCard = this;
-        carteTourner = false;
-        setTimeout(() => {
-            prevCard.classList.remove('face');
-        }, 1000);
-    }
 
+        if (carteChoix.length === 0) {
+
+            carteChoix.push(this);
+        } else if (carteChoix.length === 1) {
+
+            carteChoix.push(this);
+            setTimeout(matchCarte, 500);
+        }
 
 }
 
 
+function matchCarte() {
+    //console.log(carteChoix.length);
+    if (carteChoix[0].innerHTML === carteChoix[1].innerHTML) {
+        console.log('cest bon');
+        carteChoix[0].removeEventListener('click', flipCard);
+        carteChoix[1].removeEventListener('click', flipCard);
+        carteChoix = [];
+        carteTourner = false;
+    } else {
+        console.log('pas bon');
+        carteChoix.forEach(card => card.classList.remove('face'));
+        carteChoix = [];
+        carteTourner = false;
+    }
+}
 
 
-
-
-/*if (!carteTourner) {
-       console.log('first')
-       carteTourner = true;
-       currCard = this;
-       carteChoix.push(currCard);
-       console.log('carte 1 ' + carteChoix);
-       return;
-   } else {
-   console.log('second')
-   carteTourner = false;
-   prevCard = this;
-   carteChoix.push(prevCard);
-   console.log('carte 2 ' + carteChoix);
-   return;
-   }*/
