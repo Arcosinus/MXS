3d1970f78f5ecd15cc53f0eead495c85a7a7e8dc
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-01-05 09:23:24 +0100

    Carte comparaison (NOT FIX)

--- Memory/appMemory.js
+++ Memory/appMemory.js
@@ -1,36 +1,47 @@
-let selectCarte = document.querySelectorAll('.double-face');
-let selectAllcarte = document.querySelectorAll('.carte');
+const allCarte = document.querySelectorAll('.carte');
+const devoile = document.querySelectorAll('.double-face');
 
-for (let cartes of selectCarte){
-    cartes.addEventListener('click', tournerCarte);
-    let carteRetourner = false;
-    let carte_selected1;
-    let carte_selected2;
-    let carteChoix = [];
-    let secondClick = false;
-    function tournerCarte() {
-        if(!carteRetourner){
-            console.log('carte 1');
-            this.classList.toggle('face');
-            carteRetourner = true;
-            carte_selected1 = this;
-            carteChoix.push(carte_selected1);
-        }
-        if(!secondClick){
-            console.log('secondclick');
-            carteRetourner = false;
-            carte_selected2 = this;
-            if(carte_selected2 !== carte_selected1){
-                this.classList.toggle('arriere');
-            }
+let carteTourner = false;
+let currCard;
+let prevCard;
+let carteChoix = [];
+
+devoile.forEach(card => card.addEventListener('click', flipCard));
+
+function flipCard() {
+    this.classList.toggle('face');
+    if (!carteTourner) {
+        console.log('first')
+        carteTourner = true;
+        currCard = this;
+        carteChoix.push(currCard);
+        console.log('carte 1 ' + carteChoix);
+        return;
+    }
+    else {
+    console.log('second')
+    carteTourner = false;
+    prevCard = this;
+    carteChoix.push(prevCard);
+    console.log('carte 2 ' + carteChoix);
+    return;
+    }
+    carteTourner = false;
+    if(carteChoix.length === 2){
+        if(carteChoix[0].dataset.attr === carteChoix[1].dataset.attr){
+            console.log('vrai');
+        }else {
+            console.log('faux');
         }
     }
 }
+
 //La carte se retourne au clic et dévoile au joueur le contenu de la carte. Celle-ci reste retournée.
 //Le choix de l’utilisateur est enregistré dans une variable.
 //Lorsque le joueur clic sur une seconde carte, celle-ci est comparée avec la précédente.
 //Si la seconde carte est la même que la précédente, elles restent retournées.
 //Si la seconde carte est différente de la première. Les deux cartes se retournent face caché.
+
 
 
 
