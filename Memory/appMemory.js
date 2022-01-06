dabf96a4e1c86536a19ff3de25b447f3f1a4bbb5
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-01-06 12:01:57 +0100

    FIX

--- Memory/appMemory.js
+++ Memory/appMemory.js
@@ -1,92 +1,89 @@
 const allCarte = document.querySelectorAll('.carte');
 const devoile = document.querySelectorAll('.double-face');
-const grille = document.querySelector('.grille');
 
-
-let divTimer = document.createElement("div");
-divTimer.className = "tmp";
+const divTimer = document.createElement("div");
+divTimer.className = "temps";
 divTimer.style.cssText = 'text-align:center';
-divTimer.innerHTML ='<h1>0:00</h1>';
+divTimer.innerHTML ='<h1>00 min :00 sec</h1>';
 document.body.insertBefore(divTimer, document.body.secondChild);
+const temps = document.querySelector('h1');
 
-const tmp = document.querySelector('h1');
-
-let min = 0o0;
-let seconds = 0o0;
-let splice = 0;
-function timer(){
-    seconds++;
-    if(seconds===59){
-        seconds = 0o0;
-        min = min + 1;
-    }
-    if(seconds===10){
-        splice = '';
-    }else{
-        if(seconds=== 0o0){
-            splice = '';
-        }
-    }
-    tmp.innerText = min + ':' + splice + seconds;
-}
-
+let minutes = 0o0;
+let secondes = 0o0;
+let secondZero = 0;
 let carteTourner = false;
 let currCard;
-let prevCard;
 let carteChoix = [];
 let carteBon = [];
 let validTimer = true;
 
+function timer(){
+    secondes ++;
+    if(secondes === 59){
+        secondes = 0o0;
+        minutes = minutes + 1;
+    }
+    if(secondes === 10){
+        secondZero = '';
+    }else{
+        if(secondes === 0o0){
+            secondZero = '';
+        }
+    }
+    temps.innerText = minutes + ' min' + ':' + secondZero + secondes + ' sec';
+}
+
+/*Fonction qui mélange la position des cartes en ordre aléatoire*/
+randomCarte();
+function randomCarte() {
+    allCarte.forEach(rand=>{
+        const cartes = [...Array(allCarte.length).keys()];
+        const randomFonct = Math.floor(Math.random() * allCarte.length);
+        rand.style.order= cartes[randomFonct];
+    })
+}
+
 devoile.forEach(card => card.addEventListener('click', flipCard));
+/*Fonction qui retourne les cartes sélectionnées et les stocke dans un tableau*/
 function flipCard() {
     this.classList.toggle('face');
-    /*démarre le timer quand l'utilisateur touche une carte*/
+    /*Démarre le timer quand l'utilisateur touche une carte*/
     if (validTimer === true){
         let counter = setInterval(function(){
             timer();
         },1000);
         validTimer = false;
     }
-
     carteTourner = true;
         currCard = this;
-
-        if (carteChoix.length === 0) {
-
-            carteChoix.push(this);
-        } else if (carteChoix.length === 1) {
-
-            carteChoix.push(this);
-            setTimeout(matchCarte, 500);
-        }
-
+    if (carteChoix.length === 0) {
+        carteChoix.push(this);
+    } else if (carteChoix.length === 1) {
+        carteChoix.push(this);
+        setTimeout(matchCarte, 500);
+    }
 }
-
-
+/*Fonction qui compare la première carte et la seconde carte sélectionnée qui sont stockée dans un tableau*/
 function matchCarte() {
-    //console.log(carteChoix.length);
+    /*Si la première carte est égale à la seconde carte, les deux cartes restent en tourné face, sinon elles restent cachées*/
     if (carteChoix[0].innerHTML === carteChoix[1].innerHTML) {
-        console.log('cest bon');
         carteChoix[0].removeEventListener('click', flipCard);
         carteChoix[1].removeEventListener('click', flipCard);
         carteChoix = [];
         carteTourner = false;
         carteBon++;
-        console.log(carteBon);
+        /*Si toutes les cartes sont tournées face, la page indique la fin du jeu et la page se recharge*/
         if (carteBon === 6) {
-            setTimeout( function(){ if (confirm('vous avez gagné !')){
-                window.location.reload(false);
+        setTimeout( function(){
+            if (confirm('vous avez gagné en ' + temps.innerHTML + ' !')){
+            window.location.reload(false);
             } else {
-            }}, 500)
-
-
-
+            }
+        }, 500)
         }
     } else {
-        console.log('pas bon');
         carteChoix.forEach(card => card.classList.remove('face'));
         carteChoix = [];
         carteTourner = false;
-
     }
 }
