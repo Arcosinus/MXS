4d89563d72450d867c25ea3140783f8fec067d51
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-01-07 08:40:17 +0100

    Interface bonus

--- Memory/appMemory.js
+++ Memory/appMemory.js
@@ -1,35 +1,66 @@
 const allCarte = document.querySelectorAll('.carte');
 const devoile = document.querySelectorAll('.double-face');
 const grille = document.querySelector('.grille');
+const faceAvant = document.querySelectorAll('.face');
+const faceArriere = document.querySelectorAll('.arriere');
 
 document.body.style.userSelect = "none";
 
 const divTimer = document.createElement("div");
-divTimer.className = "temps";
-divTimer.style.cssText = 'text-align:center';
-divTimer.innerHTML ='<h1>00 min :00 sec</h1>';
+divTimer.className = "bottom";
+divTimer.style.cssText = 'background-color: rgba(255,255,255,0.25);border-radius:10px;margin:7px;margin-top:20px;color: white;height:56px;text-align: center;'
+divTimer.innerHTML =`<span class=moves>Moves :0</span>
+<span class=bonusTmp>Bonus Temps: 0 secondes</span>
+<span class=times>00 min :00 sec</span>`;
 document.body.insertBefore(divTimer, document.body.secondChild);
-const temps = document.querySelector('h1');
+const temps = document.querySelector('.times');
+const moves = document.querySelector('.moves');
+const bonusTemps = document.querySelector('.bonusTmp');
+moves.style.cssText ='position: absolute;left: 20px;font-size: 16px;top:8.5px;';
+temps.style.cssText = 'font-size: 22px;padding-top:10px;display:block;';
+bonusTemps.style.cssText = 'position: absolute;right: 20px;font-size: 16px;top: 8.5px;';
 
-const divStart = document.createElement("div");
-divStart.className = "start";
-divStart.style.cssText = 'flex:auto ';
-document.body.insertBefore(divStart, document.body.firstChild);
+const divTop2 = document.createElement("div");
+divTop2.className = "top2";
+divTop2.style.cssText = 'border-radius:10px;margin:7px;margin-top:0px;margin-bottom:-87px;color: white;height:56px;text-align: center;'
+document.body.insertBefore(divTop2, document.body.firstChild);
 
 const btnStart = document.createElement("button");
 btnStart.className = "buttonStart";
-btnStart.innerHTML = `Start`;
-divStart.appendChild(btnStart);
+btnStart.innerHTML = `START GAME`;
+btnStart.style.cssText = 'background-color:rgba(255,255,255,0.2);border:none;border-radius:7px;font-size:15px;cursor:pointer;padding:7px ';
+divTop2.appendChild(btnStart);
 
 const btnRestart = document.createElement("button");
 btnRestart.className = "buttonRestart";
-btnRestart.style.cssText = 'display:none';
-btnRestart.innerHTML = `Restart`;
-divStart.appendChild(btnRestart);
+btnRestart.innerHTML = `RESTART GAME`;
+btnRestart.style.cssText = 'background-color:rgba(255,255,255,0.2);border:none;border-radius:7px;font-size:15px;display:none;cursor:pointer;padding:7px';
+divTop2.appendChild(btnRestart);
 
-let clickStart = document.querySelector('.buttonStart');
-let clickRestart = document.querySelector('.buttonRestart');
-let divClique = document.querySelector('.start');
+const divTop = document.createElement("div");
+divTop.className = "top";
+divTop.style.cssText = 'background-color: rgba(255,255,255,0.25);border-radius:10px;margin:7px;margin-top:0px;color: white;height:43px;text-align: center;'
+document.body.insertBefore(divTop, divTop2);
+
+const titre = document.createElement("span");
+titre.className = "titre";
+titre.innerHTML = `MEMORY GAME`;
+titre.style.cssText = 'font-size:17px;padding-top:10px;display:block;';
+divTop.appendChild(titre);
+
+const clickStart = document.querySelector('.buttonStart');
+const clickRestart = document.querySelector('.buttonRestart');
+
+devoile.forEach(double=>{
+    double.style.cssText= 'background-color:rgba(255,255,255,0.2);border:none;border-radius:13px;';
+})
+faceAvant.forEach(avant=>{
+    avant.style.cssText= 'background-color:rgba(255,255,255,0.2);border:none;border-radius:13px;';
+})
+faceArriere.forEach(arriere=>{
+    arriere.style.cssText= 'background-color:rgba(255,255,255,0.2);border:none;border-radius:13px;';
+})
+grille.style.cssText = 'background-color:rgba(255,255,255,0.2);border:none;border-radius:13px;';
 
 window.onload = function() {
     setTimeout( function(){
@@ -39,22 +70,21 @@ window.onload = function() {
         clickRestart.addEventListener('click', restartClique);
     }, 250)
 }
-
 function startClique(){
-    grille.style.display = '';
-    temps.style.display = '';
+    grille.style.display = 'grid';
+    temps.style.display = 'grid';
     clickRestart.style.display = '';
     clickStart.style.display = 'none';
 }
-
 function restartClique(){
-    grille.style.display = '';
-    temps.style.display = '';
-    clickStart.style.display = '';
+    grille.style.display = 'grid';
+    temps.style.display = 'grid';
     clickRestart.style.display = 'none';
-    window.location.reload(false);
+    window.location.reload(true);
 }
 
+let bonusSecondes = 0;
+let moveCount = 0;
 let minutes = 0;
 let secondes = 0;
 let carteTourner = false;
@@ -75,7 +105,6 @@ function timer(){
         temps.innerHTML = '0' + minutes + ' min ' + ':' + secondes + ' sec';
     }
 }
-
 /*Fonction qui mélange la position des cartes en ordre aléatoire*/
 randomCarte();
 function randomCarte() {
@@ -86,9 +115,15 @@ function randomCarte() {
     })
 }
 
+// devoile.forEach(entrer => entrer.addEventListener('mouseenter',  ()=>devoile.style.transform = "rotate(7deg)"));
+// devoile.forEach(leavee => leavee.addEventListener('mouseleave',  ()=>devoile.style.transform = "scale(0.90)"));
+
+
 devoile.forEach(card => card.addEventListener('click', flipCard));
 /*Fonction qui retourne les cartes sélectionnées et les stocke dans un tableau*/
 function flipCard() {
+    moves.innerHTML = 'Moves :'+ moveCount ;
+    grille.style.cssText= 'background-color:rgba(255,255,255,0.2);';
     this.classList.toggle('face');
     /*Démarre le timer quand l'utilisateur touche une carte*/
     if (validTimer === true){
@@ -104,9 +139,12 @@ function flipCard() {
     } else if (carteChoix.length === 1) {
         carteChoix.push(this);
         setTimeout(matchCarte, 500);
+        moveCount++;
+        console.log(moveCount)
     }
+    moves.innerHTML = 'Moves :'+ moveCount ;
+    bonusTemps.innerHTML = 'Bonus Temps: '+bonusSecondes+' secondes';
 }
-
 /*Fonction qui compare la première carte et la seconde carte sélectionnée qui sont stockée dans un tableau*/
 function matchCarte() {
     /*Si la première carte est égale à la seconde carte, les deux cartes restent en tourné face, sinon elles restent cachées*/
@@ -114,18 +152,26 @@ function matchCarte() {
         carteChoix[0].removeEventListener('click', flipCard);
         carteChoix[1].removeEventListener('click', flipCard);
         carteChoix = [];
-        carteTourner = false;
+        carteTourner = true;
         carteBon++;
+        secondes--;
+        bonusSecondes++;
+        if(secondes < 10){
+            temps.innerHTML= '0' + minutes + ' min ' + ':0' + secondes + ' sec';
+        }else{
+            temps.innerHTML = '0' + minutes + ' min ' + ':' + secondes + ' sec';
+        }
         /*Si toutes les cartes sont tournées face, la page indique la fin du jeu et la page se recharge*/
         if (carteBon === 6) {
+        grille.style.cssText= 'background-color: rgba(8, 226, 55, 0.2);';
         setTimeout( function(){
-            if (confirm('vous avez gagné en ' + temps.innerHTML + ' !')){
-                window.location.reload(false);
+            if (confirm('vous avez gagné en ' + temps.innerHTML + ' ! (Total Move : '+moves.innerHTML+' avec Bonus Temps:'+bonusTemps.innerHTML+')')){
+                window.location.reload();
             }
         }, 500)
         }
-
     } else {
+        grille.style.cssText= 'background-color: rgba(230, 21, 14, 0.2);';
         carteChoix.forEach(card => card.classList.remove('face'));
         carteChoix = [];
         carteTourner = false;
